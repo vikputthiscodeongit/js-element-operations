03248edd4c0e97ff220f8551a65c4dba8544a96e
vikputthiscodeongit <code@viktorchin.nl>
2022-03-24 13:34:25 +0100

    Move createEl to index.js & improve its property generation
    * Removes external string-operations dependency for property generation.
    * Fixes missing dash when property contains a number.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-export { default as createEl } from "./createEl";
+export default function createEl(tagName, attrs) {
+    const el = document.createElement(tagName);
+
+    if (attrs) {
+        for (let [prop, val] of Object.entries(attrs)) {
+            prop = prop.replace(/[A-Z0-9]/g, letter => `-${letter.toLowerCase()}`);
+
+            el.setAttribute(prop, val);
+        }
+    }
+
+    return el;
+}
