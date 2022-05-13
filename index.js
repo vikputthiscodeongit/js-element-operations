368ec376582acdc04dc4b323b132362df172fea4
Viktor <code@viktorchin.nl>
2022-05-13 22:53:43 +0200

    Add skipNull option

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-export default function createEl(tagName, attrs) {
+export default function createEl(tagName, attrs, skipNull = true) {
     const el = document.createElement(tagName);
 
     if (attrs) {
         for (let [prop, val] of Object.entries(attrs)) {
+            if (val === null) {
+                if (skipNull) {
+                    continue;
+                }
+
+                val = "";
+            }
+
+            // ARIAHIDDEN matcht bijv. ook.
             prop = prop.replace(/[A-Z0-9]/g, letter => `-${letter.toLowerCase()}`);
 
             el.setAttribute(prop, val);
